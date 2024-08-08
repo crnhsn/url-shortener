3b7d2e4157aeee10e841d679c3115e23270a2c47
Ciaran Hassan <ciaran@ciarans-mbp.mynetworksettings.com>
2024-08-08 03:06:22 -0400

    get api working

--- server/UrlShortener/Constants/Lengths.cs
+++ server/UrlShortener/Constants/Lengths.cs
@@ -2,5 +2,5 @@ namespace UrlShortener.Constants;
 
 public static class Lengths
 {
-    public static int SHORT_URL_LENGTH = 8; 
+    public const int SHORT_URL_LENGTH = 8; 
 }
--- server/UrlShortener/Program.cs
+++ server/UrlShortener/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using UrlShortener.Concretes.Encoders;
+using UrlShortener.Concretes.Hashing;
+using UrlShortener.Concretes.Randomness;
+using UrlShortener.Concretes.Shorteners;
+
+using UrlShortener.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
+
+const int SHORT_URL_LENGTH = UrlShortener.Constants.Lengths.SHORT_URL_LENGTH;
+
+// Register dependencies
+builder.Services.AddSingleton<IHashProvider<string, byte[]>, MD5HashProvider>();
+
+builder.Services.AddSingleton<IEncoder<byte[], string>, UrlSafeBase64Encoder>();
+
+builder.Services.AddSingleton<IRandomnessProvider<string>>(provider => new UrlSafeRandomStringProvider(SHORT_URL_LENGTH));
+
+builder.Services.AddSingleton<IShorteningProvider<string, string>>(provider =>
+    new RandomizedHashBasedShortener(SHORT_URL_LENGTH,
+        provider.GetRequiredService<IHashProvider<string, byte[]>>(),
+        provider.GetRequiredService<IEncoder<byte[], string>>(),
+        provider.GetRequiredService<IRandomnessProvider<string>>()));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/shorten/{url}", (string url, IShorteningProvider<string, string> shorteningService) =>
+{
+    string shortenedUrl = UrlShortener.Constants.BaseUrl.Stem + shorteningService.Shorten(url);
+    return Results.Ok(shortenedUrl);
+})
+.WithName("ShortenUrl");
 
 app.Run();
