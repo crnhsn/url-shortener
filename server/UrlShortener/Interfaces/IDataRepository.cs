816decae18ab33d2dcb3f9592f01dc4246b6462b
Ciaran Hassan <ciaran@ciarans-mbp.mynetworksettings.com>
2024-08-08 04:23:42 -0400

    write shortened URLs to data store and add retry logic

--- server/UrlShortener/Interfaces/IDataRepository.cs
+++ server/UrlShortener/Interfaces/IDataRepository.cs
@@ -6,7 +6,6 @@ public interface IDataRepository<T, U>
 {
     
     public Task<ReadResult<U>> TryReadAsync(T key);
-    
     public Task<bool> TryWriteAsync(T key, U value);
     public Task<bool> KeyExistsAsync(T key);
     
--- server/UrlShortener/Program.cs
+++ server/UrlShortener/Program.cs
@@ -12,6 +12,8 @@ const int SHORT_URL_LENGTH = UrlShortener.Constants.Lengths.SHORT_URL_LENGTH;
 
 const string REDIS_CONNECTION_STRING = UrlShortener.Constants.DatabaseConnectionStrings.REDIS; // todo: replace with env var
 
+const int MAX_SHORTEN_RETRIES = 5;
+
 // Register dependencies
 builder.Services.AddSingleton<IHashProvider<string, byte[]>, MD5HashProvider>();
 
@@ -31,10 +33,36 @@ var app = builder.Build();
 
 // todo: update this to post request
 // todo: add functionality for custom short link 
-app.MapGet("/shorten/{url}", (string url, IShorteningProvider<string, string> shorteningService) =>
+app.MapGet("/shorten/{urlToShorten}", async (string urlToShorten,
+        IShorteningProvider<string, string> shorteningService,
+        IDataRepository<string, string> database) =>
 {
-    string shortenedUrl = BASE_URL + shorteningService.Shorten(url);
-    
+    string shortenedUrl = BASE_URL + shorteningService.Shorten(urlToShorten);
+    bool shortUrlAlreadyExists = await database.KeyExistsAsync(shortenedUrl);
+
+    if (shortUrlAlreadyExists)
+    {
+        // todo: add some kind of retry logic? retry X times before returning 404 to client?
+        // probably need to account for various flows here - e.g., user passed in a custom url vs not
+        // if user passed in custom, and it alraedy exists, vs if user didn't pass in random one
+        int retryCount = 0;
+
+        while (shortUrlAlreadyExists && retryCount < MAX_SHORTEN_RETRIES)
+        {
+            shortenedUrl = BASE_URL + shorteningService.Shorten(urlToShorten);
+            shortUrlAlreadyExists = await database.KeyExistsAsync(shortenedUrl);
+            retryCount += 1;
+        }
+    }
+
+    // if we've exhausted the number of retries without seeing a new URL, return some kind of error
+    if (shortUrlAlreadyExists)
+    {
+        return Results.StatusCode(404); // todo: update http code here
+    }
+
+    await database.TryWriteAsync(shortenedUrl, urlToShorten);
+
     return Results.Ok(shortenedUrl);
 })
 .WithName("ShortenUrl");
