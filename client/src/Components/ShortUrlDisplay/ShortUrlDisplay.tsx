0e462c25a094b077fd137a308078ad541ace27e3
Ciaran Hassan <ciaran@ciarans-mbp.mynetworksettings.com>
2024-08-10 08:04:50 -0400

    add copy button to short URL display

--- client/src/Components/ShortUrlDisplay/ShortUrlDisplay.tsx
+++ client/src/Components/ShortUrlDisplay/ShortUrlDisplay.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Input } from '@chakra-ui/react';
+import {Button, IconButton, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { BiSolidCopy } from "react-icons/bi";
 
 interface ShortUrlDisplayProps {
   shortenedUrl: string;
@@ -7,14 +8,27 @@ interface ShortUrlDisplayProps {
 
 const ShortUrlDisplay: React.FC<ShortUrlDisplayProps> = ({ shortenedUrl }) => {
   return (
-    <Input
-      value={shortenedUrl}
-      isReadOnly
-      placeholder="Your shortened URL will appear here"
-      size="lg"
-      w="100%"
-      variant="filled"
-    />
+    <InputGroup>
+      <Input
+        value={shortenedUrl}
+        isReadOnly
+        placeholder="Your shortened URL will appear here"
+        size="lg"
+        w="100%"
+        variant="filled"
+      />
+      {shortenedUrl && (
+        <InputRightElement>
+          <IconButton
+            aria-label='Copy URL'
+            icon={<BiSolidCopy />}
+            fontSize="1.5em"
+            size="sm"
+            mt="5px"
+          />
+        </InputRightElement>
+      )}
+    </InputGroup>
   );
 };
 
