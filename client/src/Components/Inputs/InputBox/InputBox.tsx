d5b4b170c327f3257514d4f4914a4d2733a55af6
Ciaran Hassan <ciaran@ciarans-mbp.mynetworksettings.com>
2024-08-10 09:35:04 -0400

    add client side validation and visual cues

--- client/src/Components/Inputs/InputBox/InputBox.tsx
+++ client/src/Components/Inputs/InputBox/InputBox.tsx
@@ -1,16 +1,27 @@
 import React from "react"; 
-import {Input} from '@chakra-ui/react';
+import {FormControl, FormErrorMessage, Input} from '@chakra-ui/react';
 
 interface InputBoxProps {
     placeholderText : string,
     onChange : (e: React.ChangeEvent<HTMLInputElement>) => void,
     onKeyDown : (e: React.KeyboardEvent<HTMLInputElement>) => void,
-    className : string
+    className : string,
+    isInvalid? : boolean,
+    errorMessage? : string
 }
-const InputBox: React.FC<InputBoxProps> = ({ placeholderText, onChange, onKeyDown, className }) => {
+const InputBox: React.FC<InputBoxProps> = ({ placeholderText, onChange, onKeyDown, className, isInvalid, errorMessage }) => {
     return (
         <div className={className}>
-            <Input placeholder={placeholderText} onChange={onChange} onKeyDown={onKeyDown} />
+            <FormControl isInvalid={isInvalid}>
+                <Input placeholder={placeholderText}
+                       onChange={onChange}
+                       onKeyDown={onKeyDown}
+                       isInvalid={isInvalid}
+                       borderColor={isInvalid ? "red.500" : "gray.200"}
+                />
+                <FormErrorMessage>{errorMessage}</FormErrorMessage>
+            </FormControl>
+
         </div>
     );
 };
--- client/src/Components/UrlShortener/UrlShortener.tsx
+++ client/src/Components/UrlShortener/UrlShortener.tsx
@@ -13,6 +13,23 @@ const UrlShortener : React.FC = () => {
     const [customAlias, setCustomAlias] = useState("");
     const [shortenedUrl, setShortenedUrl] = useState("");
     
+    // on initial load - url box is empty, so it's invalid; custom alias box is empty, which is valid
+    // but we still both to true / valid, because otherwise initial load would show an error underneath
+    // the url box
+    const [urlToShortenValid, setUrlToShortenValid] = useState(true);
+    const [customAliasValid, setCustomAliasValid] = useState(true);
+
+    const [urlErrorMessage, setUrlErrorMessage] = useState("");
+    const [aliasErrorMessage, setAliasErrorMessage] = useState("");
+
+    const MAX_ALIAS_LENGTH = 20; // todo - lift this into env var somehow, should sync with server max length
+    const ALPHANUMERIC_REGEX = /^[a-zA-Z0-9]+$/;
+
+    const MAX_URL_LENGTH = 2048; // todo - lift this into env var somehow, should sync with server max length
+    const URL_REGEX = /^(?:(?:https?|http):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/;
+
+
+
     const handleUrlToShortenChange = (e : ChangeEvent<HTMLInputElement>) => {
         
         setUrlToShorten(e.target.value);
@@ -34,49 +51,73 @@ const UrlShortener : React.FC = () => {
     }
 
     const handleSubmit = async () => {
-        console.log("called handle submit");
-        if (!urlIsValid(urlToShorten)) {
+        const urlInvalid = !urlIsValid(urlToShorten);
+        const aliasInvalid = !customAliasIsValid(customAlias);
 
-            // display error
-            return; // exit function, todo 
+        if (urlInvalid)
+        {
+            setUrlToShortenValid(false);
+            setUrlErrorMessage("Invalid URL. Please enter a valid URL, starting with http or https.");
+        }
+        else
+        {
+            setUrlToShortenValid(true);
+            setUrlErrorMessage("");
         }
 
-        if (!customAliasIsValid(customAlias)) {
-            // display error
-            return; // exit function, todo
+        if (aliasInvalid)
+        {
+            setCustomAliasValid(false);
+            setAliasErrorMessage(`Custom alias must be alphanumeric and ${MAX_ALIAS_LENGTH} characters or fewer.`);
+        }
+        else {
+            setCustomAliasValid(true);
+            setAliasErrorMessage("");
+        }
+
+        if (urlInvalid || aliasInvalid) {
+            setShortenedUrl(""); // an input failure should reset the URL display
+            return; // don't move on and execute the request if either input is invalid
         }
 
         try {
-
+            // if we get to this point, then the inputs are valid
+            // so execute the request
             const response = await shortenUrl(urlToShorten, customAlias);
-
-            // todo: validate response
             setShortenedUrl(response);
 
+        } catch (error) {
+            // todo: handle errors
+            console.error("An error occurred:", error);
         }
-        catch (error) {
-            // todo: fill this out
-        }
-    }
+    };
 
-    const urlIsValid = (longUrl : string) : boolean => {
-        return true;
-        // implement
+
+
+    const urlIsValid = (url : string) : boolean => {
+        if (url.length > MAX_URL_LENGTH) {
+            return false;
+        }
+
+        return URL_REGEX.test(url);
     }
 
     const customAliasIsValid = (customAlias : string) : boolean => {
-        return true;
-        // implement
+
+        if (!customAlias) {
+            return true; // custom alias is optional, so if it's empty, it's valid
+        }
+
+        if (customAlias.length > MAX_ALIAS_LENGTH) {
+
+            return false;
+
+        }
+
+        let isAlphanumeric : boolean = ALPHANUMERIC_REGEX.test(customAlias);
+
+        return isAlphanumeric;
     }
-
-
-
-    // todo:
-    // attach keypress event listeners to both input text boxes, to invoke submitRequest
-    // clicking on button calls onSubmitButtonClick, which calls submitRequest
-    // submitRequest calls validateInput on both boxes
-    // it then sends POST request to backend
-    // errors are caught and processed accordingly by some kind of error handler
 
 
     return (
@@ -88,11 +129,14 @@ const UrlShortener : React.FC = () => {
                   <Heading size='md' textAlign="center">URL Shortener</Heading>
                 </CardHeader>
                 <VStack spacing={4} p={4}>
+
                   <InputBox
                     placeholderText="link to shorten"
                     onChange={handleUrlToShortenChange}
                     onKeyDown={handleKeyDown}
                     className="UrlInputBox"
+                    isInvalid={!urlToShortenValid}
+                    errorMessage={urlErrorMessage}
                   />
 
                   <InputBox
@@ -100,6 +144,8 @@ const UrlShortener : React.FC = () => {
                     onChange={handleCustomAliasChange}
                     onKeyDown={handleKeyDown}
                     className="CustomAliasInputBox"
+                    isInvalid={!customAliasValid}
+                    errorMessage={aliasErrorMessage}
                   />
 
                   <Button
