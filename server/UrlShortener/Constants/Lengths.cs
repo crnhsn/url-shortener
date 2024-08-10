315d67fc85d66c4747bae353f68b5951926ca19c
crnhsn <ciarhass20@gmail.com>
2024-08-10 01:41:19 -0400

    Complete v1 of expansion / redirection endpoint (#8)

--- server/UrlShortener/Constants/Lengths.cs
+++ server/UrlShortener/Constants/Lengths.cs
@@ -2,5 +2,6 @@ namespace UrlShortener.Constants;
 
 public static class Lengths
 {
-    public const int SHORT_URL_LENGTH = 8; 
+    public const int SHORT_URL_LENGTH = 8;
+    public const int MAX_LONG_URL_LENGTH = 2048;
 }
--- server/UrlShortener/Endpoints/UrlExpansionEndpoint.cs
+++ server/UrlShortener/Endpoints/UrlExpansionEndpoint.cs
@@ -1,32 +1,70 @@
-using UrlShortener.InputValidation;
+using System.ComponentModel.DataAnnotations;
 using UrlShortener.Interfaces;
 
 namespace UrlShortener.Endpoints;
 
 public static class UrlExpansionEndpoint
 {
+    private static class ErrorMessages
+    {
+        public const string ShortCodeMissing = "SHORT_CODE_MISSING";
+        public const string ShortCodeIsNotAlphanumeric = "SHORT_CODE_FORMAT";
+        public const string ShortCodeTooLong = "SHORT_CODE_LENGTH";
+
+        public const string ShortCodeNotFound = "SHORT_CODE_NOT_FOUND";
+
+        public const string InternalServerError = "INTERNAL_SERVER_ERROR";
+
+
+    }
+    private class ExpansionRequest
+    {
+        [Required(ErrorMessage = ErrorMessages.ShortCodeMissing)]
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = ErrorMessages.ShortCodeIsNotAlphanumeric)]
+        [StringLength(Constants.Lengths.MAX_LONG_URL_LENGTH, ErrorMessage = ErrorMessages.ShortCodeTooLong)]
+        public string ShortCode {get; set;}
+
+        public ExpansionRequest(string shortCode)
+        {
+            ShortCode = shortCode;
+        }
+    }
     
     public static void MapUrlExpansionEndpoint(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/{shortUrl}", async (string shortUrl, IUrlShortenerService urlShortener) =>
+        app.MapGet("/{shortCode}", async (string shortCode, IUrlShortenerService urlShortener) =>
         {
-            // todo: add validation for incoming strings, etc.
-            // e.g., can't expand short URLs that don't have the expected base URL
-            // probably a library to do this
-    
             try
             {
-                string longUrl = await urlShortener.ResolveShortUrl(shortUrl);
-                bool urlIsValid = UrlValidator.IsValidUrl(longUrl, out string validatedUrl);
-                if (!urlIsValid)
+                // bind the incoming short code to a model so validation of the short code
+                // can just use existing model validation flow instead of needing custom flow
+                
+                var expansionRequest = new ExpansionRequest(shortCode);
+
+                var validationContext = new ValidationContext(expansionRequest);
+                var validationResults = new List<ValidationResult>();
+                bool isValid = Validator.TryValidateObject(expansionRequest,
+                                                           validationContext,
+                                                           validationResults,
+                                                           validateAllProperties:true);
+
+                if (!isValid)
                 {
-                    return Results.StatusCode(404); // not found or invalid
+                    List<string?> errorMessages = validationResults.Select(r => r.ErrorMessage).ToList();
+                    return Results.BadRequest(errorMessages);
                 }
-                return Results.Redirect(validatedUrl);
+
+                string longUrl = await urlShortener.ResolveShortUrl(shortCode);
+
+                return Results.Redirect(longUrl);
             }
             catch (Exception ex)
             {
-                return Results.StatusCode(500);
+                // todo: log exception?
+                // todo - catch custom exceptions here and send 404 if Resolve above doesn't find anything
+
+                return Results.Problem(ErrorMessages.InternalServerError,
+                                       statusCode: StatusCodes.Status500InternalServerError);
             }
     
         }).WithName("ExpandUrl");
