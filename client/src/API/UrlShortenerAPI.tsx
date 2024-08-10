9ff7be592168986d15923628d8c7305faa5c6525
Ciaran Hassan <ciaran@ciarans-mbp.mynetworksettings.com>
2024-08-10 11:16:19 -0400

    get UI responsive to server-side error messages

--- client/src/API/UrlShortenerAPI.tsx
+++ client/src/API/UrlShortenerAPI.tsx
@@ -1,26 +1,22 @@
-import axios from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 
 const BASE_URL = process.env.REACT_APP_URL_SHORTENER_API_BASE_URL;
 
-const api = axios.create({baseURL : BASE_URL});
+const api = axios.create({ baseURL: BASE_URL,
+                           validateStatus: () => true});
 
-export const shortenUrl = async (urlToShorten : string, customAlias? : string) : Promise<string>  => {
-    
-    try {
-            const response = await api.post('/shorten',
-                                              {
-                                                  longUrl: urlToShorten,
-                                                  customAlias: customAlias || undefined
-                                              });
+export const shortenUrl = async (urlToShorten: string, customAlias?: string): Promise<AxiosResponse> => {
+    try
+    {
+        const response = await api.post('/shorten', {
+            longUrl: urlToShorten,
+            customAlias: customAlias || undefined
+        });
+        return response;
+    }
 
-            return response.data;
-
-        }
-        
-        catch (error) {
-            console.error(error);
-            throw error; 
-        }
-    
-}
-
+    catch (error)
+    {
+        throw error; 
+    }
+};
--- client/src/Components/UrlShortener/UrlShortener.tsx
+++ client/src/Components/UrlShortener/UrlShortener.tsx
@@ -6,40 +6,64 @@ import InputBox from '../Inputs/InputBox/InputBox';
 import {shortenUrl} from "../../API/UrlShortenerAPI";
 import ShortUrlDisplayWithCopy from '../ShortUrlDisplay/ShortUrlDisplayWithCopy';
 
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
 
 const UrlShortener : React.FC = () => {
-    
+
     const [urlToShorten, setUrlToShorten] = useState("");
     const [customAlias, setCustomAlias] = useState("");
     const [shortenedUrl, setShortenedUrl] = useState("");
+
     
-    // on initial load - url box is empty, so it's invalid; custom alias box is empty, which is valid
-    // but we still both to true / valid, because otherwise initial load would show an error underneath
-    // the url box
     const [urlToShortenValid, setUrlToShortenValid] = useState(true);
     const [customAliasValid, setCustomAliasValid] = useState(true);
 
     const [urlErrorMessage, setUrlErrorMessage] = useState("");
     const [aliasErrorMessage, setAliasErrorMessage] = useState("");
 
+    // todo - put all these constants into a file and sync them with server somehow
+    // maybe a json config file that both the JS and C# read?
     const MAX_ALIAS_LENGTH = 20; // todo - lift this into env var somehow, should sync with server max length
     const ALPHANUMERIC_REGEX = /^[a-zA-Z0-9]+$/;
 
     const MAX_URL_LENGTH = 2048; // todo - lift this into env var somehow, should sync with server max length
     const URL_REGEX = /^(?:(?:https?|http):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/;
 
+    // error messages the server sends back - todo - lift into file / env and sync with server
+    const URL_NOT_PROVIDED = "URL_REQUIRED";
+    const URL_INVALID = "URL_INVALID";
+    const URL_TOO_LONG = "URL_LENGTH";
+
+    const CUSTOM_ALIAS_NOT_ALPHANUMERIC = "CUSTOM_ALIAS_FORMAT";
+    const CUSTOM_ALIAS_TOO_LONG = "CUSTOM_ALIAS_LENGTH";
+    const CUSTOM_ALIAS_UNAVAILABLE = "CUSTOM_ALIAS_UNAVAILABLE";
+
+    const UNEXPECTED_ERROR = "INTERNAL_SERVER_ERROR";
+
+    // error messages client displays - todo - lift into file / env? maybe not env
+    const URL_NOT_PROVIDED_MESSAGE = "URL is required. Please enter a URL starting with http or https.";
+    const URL_INVALID_MESSAGE = "The provided URL is invalid. Please enter a valid URL.";
+    const URL_TOO_LONG_MESSAGE = "The provided URL is too long. Please shorten the URL.";
+
+    const CUSTOM_ALIAS_NOT_ALPHANUMERIC_MESSAGE = "The custom alias must be alphanumeric. Please choose another alias.";
+    const CUSTOM_ALIAS_TOO_LONG_MESSAGE = `The custom alias must be ${MAX_ALIAS_LENGTH} characters or fewer.`;
+    const CUSTOM_ALIAS_UNAVAILABLE_MESSAGE = "The provided alias is not available. Please select another.";
+
+    const UNEXPECTED_ERROR_MESSAGE = "An unexpected error occurred. Please try again later.";
+
 
 
     const handleUrlToShortenChange = (e : ChangeEvent<HTMLInputElement>) => {
-        
+
         setUrlToShorten(e.target.value);
     }
-    
+
     const handleCustomAliasChange = (e : ChangeEvent<HTMLInputElement>) => {
-        
+
         setCustomAlias(e.target.value);
     }
-    
+
     const handleSubmitButtonClick = () => {
         handleSubmit();
     }
@@ -54,23 +78,18 @@ const UrlShortener : React.FC = () => {
         const urlInvalid = !urlIsValid(urlToShorten);
         const aliasInvalid = !customAliasIsValid(customAlias);
 
-        if (urlInvalid)
-        {
+        if (urlInvalid) {
             setUrlToShortenValid(false);
             setUrlErrorMessage("Invalid URL. Please enter a valid URL, starting with http or https.");
-        }
-        else
-        {
+        } else {
             setUrlToShortenValid(true);
             setUrlErrorMessage("");
         }
 
-        if (aliasInvalid)
-        {
+        if (aliasInvalid) {
             setCustomAliasValid(false);
             setAliasErrorMessage(`Custom alias must be alphanumeric and ${MAX_ALIAS_LENGTH} characters or fewer.`);
-        }
-        else {
+        } else {
             setCustomAliasValid(true);
             setAliasErrorMessage("");
         }
@@ -81,17 +100,88 @@ const UrlShortener : React.FC = () => {
         }
 
         try {
-            // if we get to this point, then the inputs are valid
-            // so execute the request
-            const response = await shortenUrl(urlToShorten, customAlias);
-            setShortenedUrl(response);
+            // if we are at this point
+            // the inputs are valid
+            // so reset the state that tracks validity
+            // and execute api call
+            setUrlErrorMessage("");
+            setAliasErrorMessage("");
+            setCustomAliasValid(true);
+            setUrlToShortenValid(true);
 
-        } catch (error) {
-            // todo: handle errors
-            console.error("An error occurred:", error);
+            // api call here has 3 potential states:
+            // success (200 ok)
+            // got to server, but server failed request (validation, etc.)
+            // failed even before getting to server, or some other way
+            // first two are encapsulated in the response
+            // third one gets thrown up to this level
+            const response = await shortenUrl(urlToShorten, customAlias);
+
+            // this if / else handles the first two - if request succeeded
+            // set the shortened url
+
+            if (response.status === 200)
+            {
+                setShortenedUrl(response.data);
+            }
+                // if request got to server but failed, extract error info from request
+                // and handle
+            else
+            {
+                console.log("got here");
+                handleError(response.data);
+            }
+        }
+        catch (error) // third case would get here
+        {
+            // todo - handle
         }
     };
 
+    // todo: extract the error messages out into constants file
+
+    const handleError = (errorMessage: string) => {
+        switch (errorMessage)
+        {
+            case URL_NOT_PROVIDED:
+                setUrlToShortenValid(false);
+                setUrlErrorMessage(URL_NOT_PROVIDED_MESSAGE);
+                break;
+
+            case URL_INVALID:
+                setUrlToShortenValid(false);
+                setUrlErrorMessage(URL_INVALID_MESSAGE);
+                break;
+
+            case URL_TOO_LONG:
+                setUrlToShortenValid(false);
+                setUrlErrorMessage(URL_TOO_LONG_MESSAGE);
+                break;
+
+            case CUSTOM_ALIAS_NOT_ALPHANUMERIC:
+                setCustomAliasValid(false);
+                setAliasErrorMessage(CUSTOM_ALIAS_NOT_ALPHANUMERIC_MESSAGE);
+                break;
+
+            case CUSTOM_ALIAS_TOO_LONG:
+                setCustomAliasValid(false);
+                setAliasErrorMessage(CUSTOM_ALIAS_TOO_LONG_MESSAGE);
+                break;
+
+            case CUSTOM_ALIAS_UNAVAILABLE:
+                setCustomAliasValid(false);
+                setAliasErrorMessage(CUSTOM_ALIAS_UNAVAILABLE_MESSAGE);
+                break;
+
+            case UNEXPECTED_ERROR:
+                setUrlErrorMessage(UNEXPECTED_ERROR_MESSAGE);
+                break;
+
+            default:
+                setUrlErrorMessage(UNEXPECTED_ERROR_MESSAGE);
+                break;
+        }
+    };
 
 
     const urlIsValid = (url : string) : boolean => {
