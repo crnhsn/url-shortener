490442a4ff9263f860e6e7885bbf8d826436d920
crnhsn <ciarhass20@gmail.com>
2024-08-09 21:58:10 -0400

    Complete Shorten endpoint v1 implementation (#7)

--- server/UrlShortener/Endpoints/UrlShortenEndpoint.cs
+++ server/UrlShortener/Endpoints/UrlShortenEndpoint.cs
@@ -1,19 +1,58 @@
-using UrlShortener.InputValidation;
+using System.ComponentModel.DataAnnotations;
 using UrlShortener.Interfaces;
 
 namespace UrlShortener.Endpoints;
 
 public static class UrlShortenEndpoint
 {
+    private static class ErrorMessages
+    {
+        public const string UrlNotProvided = "URL_REQUIRED";
+        public const string UrlInvalid = "URL_INVALID";
+
+        public const string CustomAliasNotAlphanumeric = "CUSTOM_ALIAS_FORMAT";
+        public const string CustomAliasTooLong = "CUSTOM_ALIAS_LENGTH";
+        public const string CustomAliasUnavailable = "CUSTOM_ALIAS_UNAVAILABLE";
+
+        public const string InternalServerError = "INTERNAL_SERVER_ERROR";
+
+    }
+
+    private class ShortenRequest
+    {
+        [Required(ErrorMessage = ErrorMessages.UrlNotProvided)]
+        [Url(ErrorMessage = ErrorMessages.UrlInvalid)]
+        public string LongUrl {get; set;}
+
+        [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = ErrorMessages.CustomAliasNotAlphanumeric)]
+        [StringLength(Constants.Lengths.SHORT_URL_LENGTH, ErrorMessage = ErrorMessages.CustomAliasTooLong)]
+        public string? CustomAlias {get; set;}
+    }
+
     public static void MapUrlShortenEndpoint(this IEndpointRouteBuilder app)
     {
-        
-        app.MapPost("/shorten", async (string longUrl, string? customAlias, IUrlShortenerService urlShortener) =>
+        app.MapPost("/shorten", async (ShortenRequest shortenRequest, IUrlShortenerService urlShortener) =>
             {
-                // todo: add validation for incoming strings, etc.
-            
                 try
                 {
+                    // validate incoming POST request params via model validation
+                    var validationContext = new ValidationContext(shortenRequest);
+                    var validationResults = new List<ValidationResult>();
+
+                    bool isValid = Validator.TryValidateObject(shortenRequest,
+                                                               validationContext,
+                                                               validationResults,
+                                                               validateAllProperties:true);
+
+                    if (!isValid)
+                    {
+                        List<string?> errorMessages = validationResults.Select(r => r.ErrorMessage).ToList();
+                        return Results.BadRequest(errorMessages);
+                    }
+
+                    string longUrl = shortenRequest.LongUrl;
+                    string? customAlias = shortenRequest.CustomAlias;
+
                     bool userHasRequestedCustomAlias = !String.IsNullOrWhiteSpace(customAlias);
             
                     if (userHasRequestedCustomAlias)
@@ -22,27 +61,22 @@ public static class UrlShortenEndpoint
             
                         if (!isCustomAliasAvailable)
                         {
-                            return Results.Ok("url already taken"); // todo: update error handling / status code here, maybe throw custom exception for duplicate custom code that global exception handler catches and returns to client
+                            return Results.Conflict(ErrorMessages.CustomAliasUnavailable);
                         }
                     }
 
-                    bool urlIsValid = UrlValidator.IsValidUrl(longUrl, out string validatedLongUrl);
+                    string shortenedUrl = await urlShortener.CreateShortUrl(longUrl, customAlias);
                     
-                    if (!urlIsValid)
-                    {
-                        return Results.Ok("invalid input url"); // todo: update error handling, status code, etc. here - throw custom?
-                    }
-            
-                    string shortenedUrl = await urlShortener.CreateShortUrl(validatedLongUrl, customAlias);
                     return Results.Ok(shortenedUrl);
                 }
                 catch (Exception ex)
                 {
-                    return Results.Ok(ex.ToString()); // todo: update error handling / status code
+                    // todo: log exception?
+
+                    return Results.Problem(ErrorMessages.InternalServerError);
                 }
             })
             .WithName("ShortenUrl");
-            
         
     }
     
