19474d15966a2ac47bd90b76b76a7f2bcc4038a4
Ciaran Hassan <ciaran@ciarans-mbp.mynetworksettings.com>
2024-08-11 23:14:01 -0400

    change name, add tagline, add animation

--- client/src/Components/MainComponents/UrlShortenerMainComponent.tsx
+++ client/src/Components/MainComponents/UrlShortenerMainComponent.tsx
@@ -4,7 +4,9 @@ import {MAX_URL_LENGTH, MAX_ALIAS_LENGTH} from "../../Constants/Lengths";
 
 const UrlShortenerMainComponent : React.FC = () => {
     
-    const heading = "URL Shortener"; 
+    const heading = "ShortTake";
+    const tagline = "Shorten a long URL. Like a director's cut. But with links!";
+
 
     const URL_REGEX = /^(?:(?:https?|http):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/;
     const ALIAS_REGEX = /^[a-zA-Z0-9]+$/;
@@ -41,7 +43,9 @@ const UrlShortenerMainComponent : React.FC = () => {
             maxAliasLength={MAX_ALIAS_LENGTH}
             urlValidator={urlIsValid}
             aliasValidator={customAliasIsValid}
-            componentHeading={heading}/>
+            componentHeading={heading}
+            componentTagline={tagline}/>
+
     </div>);
 }
 
